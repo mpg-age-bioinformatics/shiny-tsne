be48e2445455e618a40731f9aaa1a1b3a6d26694
FranziMe <f.metge88@gmail.com>
2018-11-22 17:28:00 +0100

    # 11 added tabs for input and plot settings to have it more tidy

--- LifeSpanCurves/ui.R
+++ LifeSpanCurves/ui.R
@@ -25,62 +25,70 @@ shinyUI(fluidPage(
     # Sidebar panel for inputs ----
     sidebarPanel(
       
-      # Input: Select a file ----
-      fileInput("uploaded_file", "Choose CSV File",
-                multiple = TRUE,
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
-      
-      # Input: Checkbox if file has header ----
-      a(href = "https://github.com/mpg-age-bioinformatics/shiny/blob/master/LifeSpanCurves/lifespan_test.csv", "Example input"),
-      checkboxInput("header", "Header", TRUE),
-      
-      # Input: Select separator ----
-      radioButtons("sep", "Separator",
-                   choices = c(Semicolon = ";",
-                               Comma = ",",
-                               Tab = "\t"),
-                   selected = ",", inline = TRUE),
-      
-      
-      # Horizontal line ----
-      tags$hr(),
-      tags$h3("Select input and model for survival analysis", align = 'left'),
-      
-      # Select variables to display ----
-      uiOutput("day"),
-      uiOutput("deaths"),
-      uiOutput("censors"),
-      uiOutput("factors"),
-      hr(),
-      # make seperate tab
-      tags$h3("Change plot settings:", align = 'left'),
-      textInput('main', 'Plot title', value = 'Survival Analysis'),
-      textInput('xlab', 'x-axis label', value = 'Time [Days]'),
-      textInput('ylab', 'y-axis label', value = 'Survival'),
-      
-      sliderInput('main.size', 'Select font size for the title', min = 0, max = 5, value = 2, step = 0.1),
-      sliderInput('lab.size', 'Select font size for the axis labels', min = 0, max = 5, value = 1.3, step = 0.1),
-      sliderInput('axis.size', 'Select font size for the axis', min = 0, max = 5, value = 1.2, step = 0.1),
-      sliderInput('legend.size', 'Select font size for the legend', min = 0, max = 5, value = 1.3, step = 0.1),
-      sliderInput('linewidth', 'Select line width', min = 0, max = 5, value = 1, step = 0.1),
-      sliderInput('margin.size', 'Select margin size', min = 1, max = 10, value = 4, step = 0.1),
-
-      textInput('colors', "Comma seperated list of colors for frist variable", value = 'black'),
-      a(href = "http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf", "R-colors"),
-      checkboxGroupInput("linetype", "Select line type for second variable:", choices = c("solid" = 1,"dashed" = 2, "dotted" = 3, 
-                                                                "dot-dashed" = 4, "longdashed" = 5, "blank" = 0), inline = TRUE, selected = 1),
-      radioButtons('logaxis', 'Use log-scale', choices = c('none' ='', 'x-axis' = 'x', 'y-axis' = 'y', 'both' = 'xy'), inline = TRUE),
-      radioButtons('conf', 'Plot confidence interval', choices = c("no" = FALSE, "yes" = TRUE), inline = TRUE),
-      radioButtons('marks', 'Mark censored events', choices = c("no" = FALSE, "yes" = TRUE), inline = TRUE),
-      radioButtons('interaction_term', 'Add interaction term', choices = c("no" = FALSE, "yes" = TRUE), inline = TRUE),
-      
-      hr(),
-      checkboxInput('table', "Show table", FALSE),
-      hr(),
-      textInput("outfile", "Output file name", value="Survival"),
-      submitButton('run survival analysis')
+       tabsetPanel(
+        tabPanel("Input and model settings",
+                 # Input: Select a file ----
+                 hr(),
+                 fileInput("uploaded_file", "Choose CSV File",
+                           multiple = TRUE,
+                           accept = c("text/csv",
+                                      "text/comma-separated-values,text/plain",
+                                      ".csv")),
+                 
+                 # Input: Checkbox if file has header ----
+                 a(href = "https://github.com/mpg-age-bioinformatics/shiny/blob/master/LifeSpanCurves/lifespan_test.csv", "Example input"),
+                 checkboxInput("header", "Header", TRUE),
+                 
+                 # Input: Select separator ----
+                 radioButtons("sep", "Separator",
+                              choices = c(Semicolon = ";",
+                                          Comma = ",",
+                                          Tab = "\t"),
+                              selected = ",", inline = TRUE),
+                 
+                 
+                 # Horizontal line ----
+                 tags$hr(),
+                 # Select variables to display ----
+                 uiOutput("day"),
+                 uiOutput("deaths"),
+                 uiOutput("censors"),
+                 uiOutput("factors"),
+                 hr(),
+                 # make seperate tab
+                 
+                 textInput('colors', "Comma seperated list of colors for frist variable", value = 'black'),
+                 a(href = "http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf", "R-colors"),
+                 hr(),
+                 checkboxGroupInput("linetype", "Select line type for second variable:", choices = c("solid" = 1,"dashed" = 2, "dotted" = 3, 
+                                                                                                     "dot-dashed" = 4, "longdashed" = 5, "blank" = 0), inline = TRUE, selected = 1),
+                 radioButtons('interaction_term', 'Add interaction term', choices = c("no" = FALSE, "yes" = TRUE), inline = TRUE),
+                 
+                 hr(),
+                 checkboxInput('table', "Show table", FALSE),
+                 hr(),
+                 textInput("outfile", "Output file name", value="Survival"),
+                 submitButton('run survival analysis')
+                 
+        ),
+        tabPanel("Plot settings", 
+                 hr(),
+                 textInput('main', 'Plot title', value = 'Survival Analysis'),
+                 textInput('xlab', 'x-axis label', value = 'Time [Days]'),
+                 textInput('ylab', 'y-axis label', value = 'Survival'),
+                 
+                 sliderInput('main.size', 'Select font size for the title', min = 0, max = 5, value = 2, step = 0.1),
+                 sliderInput('lab.size', 'Select font size for the axis labels', min = 0, max = 5, value = 1.3, step = 0.1),
+                 sliderInput('axis.size', 'Select font size for the axis', min = 0, max = 5, value = 1.2, step = 0.1),
+                 sliderInput('legend.size', 'Select font size for the legend', min = 0, max = 5, value = 1.3, step = 0.1),
+                 sliderInput('linewidth', 'Select line width', min = 0, max = 5, value = 1, step = 0.1),
+                 sliderInput('margin.size', 'Select margin size', min = 1, max = 10, value = 4, step = 0.1),
+                 radioButtons('logaxis', 'Use log-scale', choices = c('none' ='', 'x-axis' = 'x', 'y-axis' = 'y', 'both' = 'xy'), inline = TRUE),
+                 radioButtons('conf', 'Plot confidence interval', choices = c("no" = FALSE, "yes" = TRUE), inline = TRUE),
+                 radioButtons('marks', 'Mark censored events', choices = c("no" = FALSE, "yes" = TRUE), inline = TRUE),
+                 submitButton('run survival analysis')
+        )
+      )
     ),
     
     # Main panel for displaying outputs ----
