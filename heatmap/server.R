fa0d1a2fb44673aa7ae8c3ef40cbc01fd70c7d3e
FranziMe <f.metge88@gmail.com>
2018-12-05 18:22:15 +0100

    #2 finished shiny heatmap

--- heatmap/server.R
+++ heatmap/server.R
@@ -21,6 +21,7 @@ gitversion <- function(){
 library(shiny)
 library(xlsx)
 library(gplots)
+library(DT)
 
 shinyServer(function(input, output, session) {
   
@@ -59,6 +60,17 @@ shinyServer(function(input, output, session) {
                 choices = c("row.names", names(df())))
   })
   
+  
+  output$filter_cluster <- renderUI({
+    selectInput(inputId = "filter_cluster", 
+                label = "Select cluster for subsetting", 
+                choices = c("all", 1:input$num_clust), 
+                multiple = TRUE, 
+                selected = "all", selectize = FALSE)
+  })
+  
+  
+  
   heatmap.data <- reactive({
     req(input$uploaded_file, input$labels)
     
@@ -87,10 +99,16 @@ shinyServer(function(input, output, session) {
     colfunc(input$num_col)
   })
   
+  genes <- reactive({
+    genes <- input$filter_row
+    genes <- gsub(' ', '', genes)
+    genes <- unlist(strsplit(genes, ','))
+  })
+  
   
   plot.heatmap <- reactive({
     req(input$uploaded_file, input$labels)
-    
+
     if(input$dendro == 'both'){
       Rowv = TRUE
       Colv = TRUE
@@ -105,8 +123,61 @@ shinyServer(function(input, output, session) {
       Colv = FALSE
     }
     # name for color key
-    
+
     heatmap.data = heatmap.data()
+
+    HM <- heatmap.2(as.matrix(heatmap.data$D),labRow = heatmap.data$labels, trace = 'none', col = plot.color(),
+                    margins = c(input$mar_bottom, input$mar_right), Rowv = Rowv, Colv = Colv, na.color = input$na.color,
+                    dendrogram = input$dendro, key = input$color_key,
+                    distfun = function(x) dist(x, method = input$dist),
+                    hclustfun = function(x) hclust(x, method = input$clust), scale = input$heat_scale,
+                    cexRow = input$cex_lab, cexCol = input$cex_lab)
+
+
+   })
+  
+  
+  plot.heatmap.filtered <- reactive({
+    req(input$uploaded_file, input$labels)
+    
+    if(is.null(input$filter_cluster) | ("all" %in% input$filter_cluster & genes() == "all")){
+      heatmap.data = heatmap.data()
+      dendro = input$dendro
+      num_clust = input$num_clust
+    } else if (input$filter_cluster != "all" & genes() == "all") {
+      carpet = carpet()
+      carpet = subset(carpet, cluster %in% input$filter_cluster)
+      heatmap.data = list(D = carpet[, 3:(ncol(carpet)-1)], labels = carpet[,1] )
+      dendro = input$dendro.sub
+      num_clust = input$num_clust.sub
+    } else if (input$filter_cluster != "all" & genes() != "all") {
+      carpet = carpet()
+      carpet = subset(carpet, cluster %in% input$filter_cluster | carpet[, input$labels] %in% genes())
+      heatmap.data = list(D = carpet[, 3:(ncol(carpet)-1)], labels = carpet[,1] )
+      dendro = input$dendro.sub
+      num_clust = input$num_clust.sub
+    } else {
+      carpet = carpet()
+      carpet = subset(carpet, carpet[, input$labels] %in% genes())
+      heatmap.data = list(D = carpet[, 3:(ncol(carpet)-1)], labels = carpet[,1] )
+      dendro = input$dendro.sub
+      num_clust = input$num_clust.sub
+    }
+    
+    if(dendro == 'both'){
+      Rowv = TRUE
+      Colv = TRUE
+    } else if (dendro == "row"){
+      Rowv = TRUE
+      Colv = FALSE
+    } else if (dendro == 'column'){
+      Rowv = FALSE
+      Colv = TRUE
+    } else {
+      Rowv = FALSE
+      Colv = FALSE
+    }
+    # name for color key
     
     HM <- heatmap.2(as.matrix(heatmap.data$D),labRow = heatmap.data$labels, trace = 'none', col = plot.color(),
                     margins = c(input$mar_bottom, input$mar_right), Rowv = Rowv, Colv = Colv, na.color = input$na.color,
@@ -114,20 +185,148 @@ shinyServer(function(input, output, session) {
                     distfun = function(x) dist(x, method = input$dist), 
                     hclustfun = function(x) hclust(x, method = input$clust), scale = input$heat_scale,
                     cexRow = input$cex_lab, cexCol = input$cex_lab)
+    
+    colors2 <- input$color2
+    colors2 <- gsub(' ', '', colors2)
+    colors2 <- unlist(strsplit(colors2, ','))
+    
+    if(num_clust > nrow(heatmap.data$D)){
+      num_clust = nrow(heatmap.data$D)
+    } else {
+      num_clust = num_clust
+    } 
+    
+    tree1 = cutree(as.hclust(HM$rowDendrogram), num_clust)
+    nofclust.height <-  length(unique(as.vector(tree1)));
+    selcol2 <- colorRampPalette(colors2)
+    clustcol.height = selcol2(nofclust.height)
+    
+    HM <- heatmap.2(as.matrix(heatmap.data$D),labRow = heatmap.data$labels, trace = 'none', col = plot.color(),
+                    margins = c(input$mar_bottom, input$mar_right), Rowv = Rowv, Colv = Colv, na.color = input$na.color,
+                    dendrogram = input$dendro, key = input$color_key, 
+                    distfun = function(x) dist(x, method = input$dist), 
+                    hclustfun = function(x) hclust(x, method = input$clust), scale = input$heat_scale,
+                    cexRow = input$cex_lab, cexCol = input$cex_lab, RowSideColors = clustcol.height[tree1])
   })
+  
+  
+  
+  
+  carpet <- reactive({
+    req(input$uploaded_file)
+    D <- plot.heatmap()
+    heatmap.data = heatmap.data()
+    
+    if(input$num_clust > nrow(heatmap.data$D)){
+      num_clust = nrow(heatmap.data$D)
+    } else {
+      num_clust = input$num_clust
+    } 
+    
+    tree1 = cutree(hclust(dist(as.matrix(heatmap.data$D), method = input$dist), method = input$clust, members = heatmap.data$labels), num_clust)
+    tree1 = data.frame(ID = heatmap.data$labels, cluster = tree1)
+    
+    H1_carpet = t(D$carpet)
+    row.names(H1_carpet) <- D$rowInd
+    H1_carpet = merge(H1_carpet, tree1, by = "row.names", sort = FALSE)
+    H1_carpet <- H1_carpet[,c("ID", "cluster", row.names(D$carpet))]
+    H1_carpet <- H1_carpet[rev(row.names(H1_carpet)),c("ID", "cluster", row.names(D$carpet))]
+    names(H1_carpet)[1] <- input$labels
+    H1_carpet$heatmap_order <- seq(1:nrow(H1_carpet))
+    H1_carpet
+    
+  })
+  
+  
+  carpet.filtered <- reactive({
+    req(input$uploaded_file)
+    D <- plot.heatmap.filtered()
+    
+    if(is.null(input$filter_cluster) | ("all" %in% input$filter_cluster & genes() == "all")){
+      heatmap.data = heatmap.data()
+      dendro = input$dendro
+      num_clust = input$num_clust
+    } else if (input$filter_cluster != "all" & genes() == "all") {
+      carpet = carpet()
+      carpet = subset(carpet, cluster %in% input$filter_cluster)
+      row.names(carpet) <- 1:nrow(carpet)
+      heatmap.data = list(D = carpet[, 3:(ncol(carpet)-1)], labels = carpet[,1] )
+      dendro = input$dendro.sub
+      num_clust = input$num_clust.sub
+    } else if (input$filter_cluster != "all" & genes() != "all") {
+      carpet = carpet()
+      carpet = subset(carpet, cluster %in% input$filter_cluster | carpet[, input$labels] %in% genes())
+      row.names(carpet) <- 1:nrow(carpet)
+      heatmap.data = list(D = carpet[, 3:(ncol(carpet)-1)], labels = carpet[,1] )
+      dendro = input$dendro.sub
+      num_clust = input$num_clust.sub
+    } else {
+      carpet = carpet()
+      carpet = subset(carpet, carpet[, input$labels] %in% genes())
+      row.names(carpet) <- 1:nrow(carpet)
+      heatmap.data = list(D = carpet[, 3:(ncol(carpet)-1)], labels = carpet[,1] )
+      dendro = input$dendro.sub
+      num_clust = input$num_clust.sub
+    }
+    
+    
+    if(input$num_clust.sub > nrow(heatmap.data$D)){
+      num_clust = nrow(heatmap.data$D)
+    } else {
+      num_clust = input$num_clust.sub
+    } 
+    
+    tree1 = cutree(hclust(dist(as.matrix(heatmap.data$D), method = input$dist), method = input$clust, members = heatmap.data$labels), num_clust)
+    tree1 = data.frame(ID = heatmap.data$labels, cluster = tree1)
+    
+    H1_carpet = t(D$carpet)
+    row.names(H1_carpet) <- D$rowInd
+    H1_carpet = merge(H1_carpet, tree1, by = "row.names", sort = FALSE)
+    H1_carpet <- H1_carpet[,c("ID", "cluster", row.names(D$carpet))]
+    H1_carpet <- H1_carpet[rev(row.names(H1_carpet)),c("ID", "cluster", row.names(D$carpet))]
+    names(H1_carpet)[1] <- input$labels
+    H1_carpet$heatmap_order <- seq(1:nrow(H1_carpet))
+    H1_carpet
+    
+  })
+  
   
   
   observe({
     output$heatmap <- renderPlot({
       req(input$uploaded_file, input$labels)
 
-      if(input$dendro == 'both'){
+      if(is.null(input$filter_cluster) | ("all" %in% input$filter_cluster & genes() == "all")){
+        heatmap.data = heatmap.data()
+        dendro = input$dendro
+        num_clust = input$num_clust
+      } else if (input$filter_cluster != "all" & genes() == "all") {
+        carpet = carpet()
+        carpet = subset(carpet, cluster %in% input$filter_cluster)
+        heatmap.data = list(D = carpet[, 3:(ncol(carpet)-1)], labels = carpet[,1] )
+        dendro = input$dendro.sub
+        num_clust = input$num_clust.sub
+      } else if (input$filter_cluster != "all" & genes() != "all") {
+        carpet = carpet()
+        carpet = subset(carpet, cluster %in% input$filter_cluster | carpet[, input$labels] %in% genes())
+        heatmap.data = list(D = carpet[, 3:(ncol(carpet)-1)], labels = carpet[,1] )
+        dendro = input$dendro.sub
+        num_clust = input$num_clust.sub
+      } else {
+        carpet = carpet()
+        carpet = subset(carpet, carpet[, input$labels] %in% genes())
+        heatmap.data = list(D = carpet[, 3:(ncol(carpet)-1)], labels = carpet[,1] )
+        dendro = input$dendro.sub
+        num_clust = input$num_clust.sub
+    }
+    
+      if(dendro == 'both'){
         Rowv = TRUE
         Colv = TRUE
-      } else if (input$dendro == "row"){
+      } else if (dendro == "row"){
         Rowv = TRUE
         Colv = FALSE
-      } else if (input$dendro == 'col'){
+      } else if (dendro == 'column'){
         Rowv = FALSE
         Colv = TRUE
       } else {
@@ -135,90 +334,105 @@ shinyServer(function(input, output, session) {
         Colv = FALSE
       }
       # name for color key
-
-      heatmap.data = heatmap.data()
       
       HM <- heatmap.2(as.matrix(heatmap.data$D),labRow = heatmap.data$labels, trace = 'none', col = plot.color(),
-              margins = c(input$mar_bottom, input$mar_right), Rowv = Rowv, Colv = Colv, na.color = input$na.color,
-              dendrogram = input$dendro, key = input$color_key, 
-              distfun = function(x) dist(x, method = input$dist), 
-              hclustfun = function(x) hclust(x, method = input$clust), scale = input$heat_scale,
-              cexRow = input$cex_lab, cexCol = input$cex_lab)
-
+                      margins = c(input$mar_bottom, input$mar_right), Rowv = Rowv, Colv = Colv, na.color = input$na.color,
+                      dendrogram = input$dendro, key = input$color_key, 
+                      distfun = function(x) dist(x, method = input$dist), 
+                      hclustfun = function(x) hclust(x, method = input$clust), scale = input$heat_scale,
+                      cexRow = input$cex_lab, cexCol = input$cex_lab)
+      
       colors2 <- input$color2
       colors2 <- gsub(' ', '', colors2)
       colors2 <- unlist(strsplit(colors2, ','))
       
-      if(input$num_clust > nrow(heatmap.data$D)){
+      if(num_clust > nrow(heatmap.data$D)){
         num_clust = nrow(heatmap.data$D)
       } else {
-        num_clust = input$num_clust
+        num_clust = num_clust
       } 
       
       tree1 = cutree(as.hclust(HM$rowDendrogram), num_clust)
       nofclust.height <-  length(unique(as.vector(tree1)));
       selcol2 <- colorRampPalette(colors2)
       clustcol.height = selcol2(nofclust.height)
-    
+      
       HM <- heatmap.2(as.matrix(heatmap.data$D),labRow = heatmap.data$labels, trace = 'none', col = plot.color(),
                       margins = c(input$mar_bottom, input$mar_right), Rowv = Rowv, Colv = Colv, na.color = input$na.color,
                       dendrogram = input$dendro, key = input$color_key, 
                       distfun = function(x) dist(x, method = input$dist), 
                       hclustfun = function(x) hclust(x, method = input$clust), scale = input$heat_scale,
                       cexRow = input$cex_lab, cexCol = input$cex_lab, RowSideColors = clustcol.height[tree1])
+
     }, height = input$plot_height, width = input$plot_width)
   })
   
   
-  carpet <- reactive({
-    req(input$uploaded_file)
-    D <- plot.heatmap()
-    heatmap.data = heatmap.data()
-      
-    if(input$num_clust > nrow(heatmap.data$D)){
-      num_clust = nrow(heatmap.data$D)
-    } else {
-      num_clust = input$num_clust
-    } 
-      
-    tree1 = cutree(hclust(dist(as.matrix(heatmap.data$D), method = input$dist), method = input$clust, members = heatmap.data$labels), num_clust)
-    tree1 = data.frame(ID = heatmap.data$labels, cluster = tree1)
-      
-    H1_carpet = t(D$carpet)
-    row.names(H1_carpet) <- D$rowInd
-    H1_carpet = merge(H1_carpet, tree1, by = "row.names", sort = FALSE)
-    H1_carpet <- H1_carpet[,c("ID", "cluster", row.names(D$carpet))]
-    H1_carpet <- H1_carpet[rev(row.names(H1_carpet)),c("ID", "cluster", row.names(D$carpet))]
-    names(H1_carpet)[1] <- input$labels
-    H1_carpet
-    
-  })
-
   
-  output$clusterMap <- renderTable({
-    if (input$table){
-      carpet()
-    }
+  output$clusterMap <- DT::renderDataTable({
+    if(input$table)
+      datatable(
+        carpet(),
+        selection = list(mode = "multiple"),
+        caption = "Heatmap Carpet",
+        options = list(pageLength = nrow(carpet()))
+      )
   })
+  
+  output$clusterMap2 <- DT::renderDataTable({
+    if(input$table)
+      datatable(
+        carpet.filtered(),
+        selection = list(mode = "multiple"),
+        caption = "Heatmap Carpet",
+        options = list(pageLength = nrow(carpet()))
+      )
+  })
+  
+  
   # download plot
-  
   output$downloadPlot <- downloadHandler(
     # specify file name
     filename = function() {
-      paste(input$outfile,".heatmap.",gitversion(),".pdf", sep = "")
+      paste(input$outfile,".plot.",gitversion(),".pdf", sep = "")
     },
     content = function(filename){
       # open device
       pdf(filename, height = (input$plot_height/10), width = (input$plot_width/10))
       
       
-      if(input$dendro == 'both'){
+      if(is.null(input$filter_cluster) | ("all" %in% input$filter_cluster & genes() == "all")){
+        heatmap.data = heatmap.data()
+        dendro = input$dendro
+        num_clust = input$num_clust
+      } else if (input$filter_cluster != "all" & genes() == "all") {
+        carpet = carpet()
+        carpet = subset(carpet, cluster %in% input$filter_cluster)
+        heatmap.data = list(D = carpet[, 3:(ncol(carpet)-1)], labels = carpet[,1] )
+        dendro = input$dendro.sub
+        num_clust = input$num_clust.sub
+      } else if (input$filter_cluster != "all" & genes() != "all") {
+        carpet = carpet()
+        carpet = subset(carpet, cluster %in% input$filter_cluster | carpet[, input$labels] %in% genes())
+        heatmap.data = list(D = carpet[, 3:(ncol(carpet)-1)], labels = carpet[,1] )
+        dendro = input$dendro.sub
+        num_clust = input$num_clust.sub
+      } else {
+        carpet = carpet()
+        carpet = subset(carpet, carpet[, input$labels] %in% genes())
+        heatmap.data = list(D = carpet[, 3:(ncol(carpet)-1)], labels = carpet[,1] )
+        dendro = input$dendro.sub
+        num_clust = input$num_clust.sub
+      }
+
+            
+      if(dendro == 'both'){
         Rowv = TRUE
         Colv = TRUE
-      } else if (input$dendro == "row"){
+      } else if (dendro == "row"){
         Rowv = TRUE
         Colv = FALSE
-      } else if (input$dendro == 'col'){
+      } else if (dendro == 'column'){
         Rowv = FALSE
         Colv = TRUE
       } else {
@@ -234,10 +448,10 @@ shinyServer(function(input, output, session) {
       colors2 <- gsub(' ', '', colors2)
       colors2 <- unlist(strsplit(colors2, ','))
       
-      if(input$num_clust > nrow(heatmap.data$D)){
+      if(num_clust > nrow(heatmap.data$D)){
         num_clust = nrow(heatmap.data$D)
       } else {
-        num_clust = input$num_clust
+        num_clust = num_clust
       } 
       
       tree1 = cutree(as.hclust(HM$rowDendrogram), num_clust)
@@ -246,11 +460,11 @@ shinyServer(function(input, output, session) {
       clustcol.height = selcol2(nofclust.height)
       
       HM <- heatmap.2(as.matrix(heatmap.data$D),labRow = heatmap.data$labels, trace = 'none', col = plot.color(),
-                      margins = c(input$mar_bottom, input$mar_right), Rowv = Rowv, Colv = Colv, na.color = input$na.color,
-                      dendrogram = input$dendro, key = input$color_key, 
+                      margins = c(input$mar_bottom * 5, input$mar_right * 5), Rowv = Rowv, Colv = Colv, na.color = input$na.color,
+                      dendrogram = dendro, key = input$color_key, 
                       distfun = function(x) dist(x, method = input$dist), 
                       hclustfun = function(x) hclust(x, method = input$clust), scale = input$heat_scale,
-                      cexRow = input$cex_lab, cexCol = input$cex_lab, RowSideColors = clustcol.height[tree1])
+                      cexRow = input$cex_lab * 5, cexCol = input$cex_lab * 5, RowSideColors = clustcol.height[tree1], keysize = 0.5, key.par = list( cex = input$cex_lab * 2 ))
       
        # close device
       dev.off()
@@ -267,6 +481,22 @@ shinyServer(function(input, output, session) {
         return(NULL)
       if (input$table){
         write.table(carpet(), filename, row.names = FALSE, quote = FALSE, sep = '\t')
+      }
+    }
+  )
+
+  
+  output$downloadTable <- downloadHandler(
+    filename = function() {
+      paste(input$outfile,".filtered_carpet.",gitversion(),".tsv", sep = "")
+    },
+    content = function(filename) {
+      inFile <- input$uploaded_file
+      
+      if (is.null(inFile))
+        return(NULL)
+      if (input$table){
+        write.table(carpet.filtered(), filename, row.names = FALSE, quote = FALSE, sep = '\t')
       }
     }
   )
--- heatmap/ui.R
+++ heatmap/ui.R
@@ -18,7 +18,7 @@ library(tidyverse)
 shinyUI(fluidPage(
   
   # App title ----
-  titlePanel(title = h1("Run a Survival Analysis", align = "left")),
+  titlePanel(title = h1("Generate a heatmap", align = "left")),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -27,9 +27,10 @@ shinyUI(fluidPage(
     sidebarPanel(
       
       tabsetPanel(
-        tabPanel("Input and model settings",
+        tabPanel("Input and cluster settings",
                  # Input: Select a file ----
                  hr(),
+                 a(href = "https://github.com/mpg-age-bioinformatics/shiny/blob/master/heatmap/heatmap_example.csv", "Example input"),
                  fileInput("uploaded_file", "Choose File",
                            accept = c("text/csv",
                                       "text/comma-separated-values,text/plain",
@@ -43,24 +44,24 @@ shinyUI(fluidPage(
                                                                                            'tab-separated' = '\t', 
                                                                                            'comma-seperated' = ',', 
                                                                                            'semicolon-separated' = ';'), inline = TRUE),
-                 a(href = "https://github.com/mpg-age-bioinformatics/shiny/blob/master/LifeSpanCurves/input_examples/lifespan_test.csv", "Example input"),
 
                  # Input: Checkbox if file has header ----
                  checkboxInput("header", "Header", TRUE),
                  uiOutput("labels"),
-                 radioButtons("dendro", "Dendrogram", choices = c("both","row","column","none"), selected = "both", inline = TRUE),
-                 textInput('na.color', 'select color for NA', value = 'black'),
-                 selectInput('clust', 'Select Cluster algortihm', choices = c("complete", "ward.D", "ward.D2", "single",  "average", "mcquitty", "median", "centroid")), 
+                 radioButtons("dendro", "Dendrogram to plot", choices = c("both","row","column","none"), selected = "both", inline = TRUE),
+                 textInput('na.color', 'Select color for NA', value = 'black'),
+                 selectInput('clust', 'Select cluster algortihm', choices = c("complete", "ward.D", "ward.D2", "single",  "average", "mcquitty", "median", "centroid")), 
                  selectInput('dist', 'Select distance', choices = c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski")),
                  sliderInput('num_clust', 'Select number of clusters', min = 1, max = 20, value = 4, step = 1),
                  radioButtons("table", "Show cluster table", choices = c("yes" = TRUE,"no" = FALSE), selected = TRUE, inline = TRUE),
+                 textInput('outfile', 'Output prefix', value = 'heatmap'),
                  
                  submitButton('generate heatmap')
                  
         ),
         tabPanel("Plot settings",
-                 textInput('color', 'color scheme', value = 'blue, white, red'),
-                 textInput('color2', 'cluster color scheme', value = 'green, white, black, blue'),
+                 textInput('color', 'Color scheme', value = 'blue, white, red'),
+                 textInput('color2', 'Cluster color scheme', value = 'green, white, black, blue'),
                  radioButtons('log_transform', 'Transform data to log scale', choices = c("none", "log10", "log2"), selected = "none", inline = TRUE),
                  radioButtons('heat_scale', 'Heatmap scaling function', choices = c("none", "row", "col"), selected = "none", inline = TRUE),
                  sliderInput('cex_lab', 'Select label size', min = 0.1, max = 3, value = 2, step = 0.1),
@@ -69,27 +70,39 @@ shinyUI(fluidPage(
                  sliderInput('mar_bottom', 'Select bottom margin', min = 1, max = 50, value = 20, step = 0.1),
                  sliderInput('plot_width', 'Select plot width', min = 200, max = 5000, value = 1000, step = 10),
                  sliderInput('plot_height', 'Select plot height', min = 200, max = 5000, value = 750, step = 10),
-                 radioButtons('color_key', 'Inlcude Color Key', choices = c('yes' = TRUE, 'no' = FALSE), selected = TRUE, inline = TRUE),
+                 radioButtons('color_key', 'Inlcude color key', choices = c('yes' = TRUE, 'no' = FALSE), selected = TRUE, inline = TRUE),
 
+                 submitButton('generate heatmap')
+                 
+            ),
+        tabPanel("Filtering",
+                 br(),
+                 uiOutput('filter_cluster'),
+                 textInput('filter_row', 'Select rows (comma seperated list of IDs)', value = 'all'),
+                 radioButtons("dendro.sub", "Dendrogram", choices = c("both","row","column","none"), selected = "both", inline = TRUE),
+                 sliderInput('num_clust.sub', 'Select number of sub clusters', min = 1, max = 20, value = 1, step = 1),
+                 
                  submitButton('generate heatmap')
                  
             )
           )
-      
         ),
         
         # Main panel for displaying outputs ----
         mainPanel(
           plotOutput("heatmap", height = 'auto', width = 'auto'),
           hr(),
-          downloadButton('downloadPlot', 'Download Plot'),
-          downloadButton('downloadTable', 'Download Table'),
-          tableOutput("clusterMap"),
-          # br(),br(),
+          downloadButton('downloadPlot', 'Download plot'),
+          downloadButton('downloadTable', 'Download table'),
+          downloadButton('downloadTable2', 'Download filtered table'),
+          DT::dataTableOutput("clusterMap2"),
+          DT::dataTableOutput("clusterMap"),
+          br(),br(),
           htmlOutput("pseudocount"),
-          p("This App uses the", code('survival'), " package. For more information read the respective documentation in ",
-            a("cran", href = "https://cran.r-project.org/web/packages/survival/index.html"),
-            "and wikipedia's entry for ", a("survival analysis.",href="https://en.wikipedia.org/wiki/Survival_analysis" )),
+          p("This App uses the", code('heatmap.2'), "function of the ", code('gplots'), " package. For more information read the respective documentation on ",
+            a("cran. ", href = "https://www.rdocumentation.org/packages/gplots/versions/3.0.1/topics/heatmap.2"), code("heatmap.2"),
+            "uses ", a("hclust",href="https://www.rdocumentation.org/packages/stats/versions/3.5.1/topics/hclust" ), " for clustering and ", 
+a("dist",href="https://www.rdocumentation.org/packages/stats/versions/3.5.1/topics/dist" ), " for calculating the distance matrix. " ),
           p("Please keep the version tag on all downloaded files."),
           htmlOutput('appversion')
         )
