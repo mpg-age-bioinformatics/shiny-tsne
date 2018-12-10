ca5b6096fb77a2777490bba128e632437f9c0454
FranziMe <f.metge88@gmail.com>
2018-12-10 19:27:35 +0100

    #16 ready to be tested and published

--- scatterplot/server.R
+++ scatterplot/server.R
@@ -9,6 +9,7 @@ gitversion <- function(){
 library(shiny)
 library(xlsx)
 library(ggplot2)
+library(tidyverse)
 
 futile.logger::flog.threshold(futile.logger::ERROR, name = "ScatterplotLogger")
 
@@ -16,10 +17,14 @@ futile.logger::flog.threshold(futile.logger::ERROR, name = "ScatterplotLogger")
 shinyServer(function(input, output, session) {
   
   # reformat input data
-  plot.data <- reactive({
-    inFile <- input$file1
+  df <- reactive({
+    req(input$uploaded_file)
+    inFile <- input$uploaded_file
     filetype <- input$filetype
-    req(inFile)
+    
+    if (is.null(inFile)){
+      return(NULL)
+    }
     
     filetype_map <- c("xlsx" = 'xlsx',  'tsv' = '\t', 'csv' = ',', 'txt'=" ")
     if(filetype == 'auto'){
@@ -37,66 +42,251 @@ shinyServer(function(input, output, session) {
     if(filetype == 'xlsx'){
       D <- read.xlsx(inFile$datapath, sheetIndex = 1, header = input$header)
     } else {
-      D <- read.csv(inFile$datapath, header = input$header, sep = filetype)
+      D <-read.csv(inFile$datapath, header = input$header, sep = filetype)
     }
-    vars <- names(D)
-    if (input$columnx == "--select--"){
-      updateSelectInput(session, "columnx","Select X-axis", choices = vars)
-    }
-    if (input$columny == "--select--"){
-      updateSelectInput(session, "columny","Select Y-axis", choices = vars)
-    }
-    req(input$columnx != "--select--", input$columny != "--select--")
-    req(input$columnx != input$columny)
-
-    D[D == ''] <- NA
-    
-    D <-D[,c(input$columnx,input$columny)]
-    
-    D <- na.omit(D)
-    x<-input$columnx
-    y<-input$columny
-    
-    D["x.coord"]<-D$x
-    D["y.coord"]<-D$y
-    
-    return(D)
   })
   
-  # online plot
-  output$scatterplot <- renderPlot({
-    D<-plot.data()
+  output$x <- renderUI({
+    selectInput(inputId = "x", 
+                label = "Select x", 
+                choices = c(NULL, names(df())), selected = NULL)
+  })
+
+  output$y <- renderUI({
+    selectInput(inputId = "y", 
+                label = "Select y", 
+                choices = c(NULL, names(df())), selected = NULL)
+  })
+
+  output$factors <- renderUI({
+    selectInput(inputId = "factors", 
+                label = "Select factor", 
+                choices = c(NULL, names(df())), selected = NULL)
+  })
+
+  # output$labels <- renderUI({
+  #   selectInput(inputId = "labels", 
+  #               label = "Select point labels", 
+  #               choices = c(NULL, names(df())), selected = NULL)
+  # })
+  
+  
+  plot.data <- reactive({
+    req(input$x, input$y)
+    D.plot <- df()
+
+    D.plot[D.plot == ''] <- NA
     
-    p <- ggplot(D, aes(x=x.coord, y=y.coord)) + theme_bw()
+    D.plot <- na.omit(D.plot)
+
+    D.plot[, "x.coord"]<-D.plot[ ,input$x]
+    D.plot[, "y.coord"]<-D.plot[ ,input$y]
+    if(input$colortype == 'single'){
+      D.plot[, 'color_code'] <- as.factor(1)
+    } else if(input$colortype == 'factor') {
+      D.plot[, "color_code"] <- as.factor(D.plot[, input$factors])
+    } else {
+      D.plot[, "color_code"] <- round(as.numeric(D.plot[, input$factors]), 2)
+    }
+    D.plot
+  })
+  
+  plot.color <- reactive({
+    D <- plot.data()
+    colors <- input$color
+    colors <- gsub(' ', '', colors)
+    colors <- unlist(strsplit(colors, ','))
+    colfunc <- colorRampPalette(colors)
+    if(input$colortype == 'single'){
+      colfunc(1)
+    } else if(input$colortype == 'factor') {
+      colfunc(length(levels(D[, 'color_code'])))
+    } else {
+      colfunc(2)
+    }
+  })
+
+  ranges1 <- reactiveValues(x = NULL, y = NULL)
+  ranges2 <- reactiveValues(x = NULL, y = NULL)
+  
+  output$plot2 <- renderPlot({
+    ggplot(mtcars, aes(wt, mpg)) +
+      geom_point()
+  })
+  
+  output$plot3 <- renderPlot({
+    ggplot(mtcars, aes(wt, mpg)) +
+      geom_point() +
+      coord_cartesian(xlim = ranges2$x, ylim = ranges2$y, expand = FALSE)
+  })
+  
+  # When a double-click happens, check if there's a brush on the plot.
+  # If so, zoom to the brush bounds; if not, reset the zoom.
+  observe({
+    brush <- input$plot1_brush
+    if (!is.null(brush)) {
+      ranges1$x <- c(brush$xmin, brush$xmax)
+      ranges1$y <- c(brush$ymin, brush$ymax)
+      
+    } else {
+      ranges1$x <- NULL
+      ranges1$y <- NULL
+    }
+  })
+  
+  observeEvent(input$plot2_dblclick, {
+    brush <- input$plot2_brush
+    if (!is.null(brush)) {
+      ranges2$x <- c(brush$xmin, brush$xmax)
+      ranges2$y <- c(brush$ymin, brush$ymax)
+      
+    } else {
+      ranges2$x <- NULL
+      ranges2$y <- NULL
+    }
+  })
+  
+  
+  output$scatterplot1 <- renderPlot({
+    D<-plot.data()
+    color <- plot.color()
+    
+    p <- ggplot(D, aes(x=x.coord, y=y.coord, color = color_code)) 
     p <- p +  geom_point(size=input$scex, shape=as.integer(input$pch) ) 
+    
+    # change color
+    if(input$colortype == 'continious'){
+      if(as.logical(input$showLegend)){
+        p <- p + scale_color_gradient(low=color[1], high=color[2], name = input$factors)
+      } else {
+        p <- p + scale_color_gradient(low=color[1], high=color[2], guide = as.logical(input$showLegend))
+      }
+    } else {
+      if(as.logical(input$showLegend)){
+        p <- p + scale_color_manual(values = color, name = input$factors)
+      } else {
+        p <- p + scale_color_manual(values = color, guide = as.logical(input$showLegend))
+      }
+    }
     p <- p + ggtitle(input$title) + theme(plot.title = element_text(hjust = 0.5)) 
     p <- p + xlab(input$xlabel) + ylab(input$ylabel)
-    xlow=layer_scales(p)$x$range$range[1]
-    xupper=layer_scales(p)$x$range$range[2]
-    ylow=layer_scales(p)$y$range$range[1]
-    yupper=layer_scales(p)$y$range$range[2]
-    if (!is.na(input$lowerx)){
-      xlow=input$lowerx
+    if(input$showGrid == TRUE){
+      p <- p + theme_bw() 
+    } else {
+      p <- p + theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
     }
-    if (!is.na(input$upperx)){
-      xupper=input$upperx
-    }
-    if (!is.na(input$lowery)){
-      ylow=input$lowery
-    }
-    if (!is.na(input$uppery)){
-      yupper=input$uppery
-    }
-    p <- p+xlim(xlow, xupper) + ylim(ylow, yupper) 
+    p <- p + theme(plot.title = element_text(hjust = 0.5, size = input$fontsize.title),
+                   axis.text = element_text(colour="black", size = input$fontsize.axis),
+                   axis.title = element_text(colour="black", size = input$fontsize.axis),
+                   legend.text = element_text(colour="black", size = input$fontsize.legend),
+                   legend.title = element_text(colour="black", size = input$fontsize.legend)
+    )
+    
+    
+    # xlow=layer_scales(p)$x$range$range[1]
+    # xupper=layer_scales(p)$x$range$range[2]
+    # ylow=layer_scales(p)$y$range$range[1]
+    # yupper=layer_scales(p)$y$range$range[2]
+    # if (!is.na(input$lowerx)){
+    #   xlow=input$lowerx
+    # }
+    # if (!is.na(input$upperx)){
+    #   xupper=input$upperx
+    # }
+    # if (!is.na(input$lowery)){
+    #   ylow=input$lowery
+    # }
+    # if (!is.na(input$uppery)){
+    #   yupper=input$uppery
+    # }
+    # p <- p+xlim(xlow, xupper) + ylim(ylow, yupper) 
     p
+  })
+  
+  
+  # online plot
+  output$scatterplot2 <- renderPlot({
+    D<-plot.data()
+    color <- plot.color()
+    
+    p <- ggplot(D, aes(x=x.coord, y=y.coord, color = color_code)) 
+    p <- p +  geom_point(size=input$scex, shape=as.integer(input$pch)) 
+    p <- p + ggtitle(input$title) + theme(plot.title = element_text(hjust = 0.5)) 
+    p <- p + xlab(input$xlabel) + ylab(input$ylabel)
+    
+    if(is.null(ranges2$x)){
+      p <- p + coord_cartesian(xlim = ranges1$x, ylim = ranges1$y, expand = FALSE)
+    } else {
+      p <- p + coord_cartesian(xlim = ranges2$x, ylim = ranges2$y, expand = FALSE)
+    }
+    
+    if(input$colortype == 'continious'){
+      if(as.logical(input$showLegend)){
+        p <- p + scale_color_gradient(low=color[1], high=color[2], name = input$factors)
+      } else {
+        p <- p + scale_color_gradient(low=color[1], high=color[2], guide = as.logical(input$showLegend))
+      }
+    } else {
+      if(as.logical(input$showLegend)){
+        p <- p + scale_color_manual(values = color, name = input$factors)
+      } else {
+        p <- p + scale_color_manual(values = color, guide = as.logical(input$showLegend))
+      }
+    }
+    
+    if(input$showGrid == TRUE){
+      p <- p + theme_bw() 
+    } else {
+      p <- p + theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+    }
+    p <- p + theme(plot.title = element_text(hjust = 0.5, size = input$fontsize.title),
+                   axis.text = element_text(colour="black", size = input$fontsize.axis),
+                   axis.title = element_text(colour="black", size = input$fontsize.axis),
+                   legend.text = element_text(colour="black", size = input$fontsize.legend),
+                   legend.title = element_text(colour="black", size = input$fontsize.legend)
+    )
+    
+    p
+    # xlow=layer_scales(p)$x$range$range[1]
+    # xupper=layer_scales(p)$x$range$range[2]
+    # ylow=layer_scales(p)$y$range$range[1]
+    # yupper=layer_scales(p)$y$range$range[2]
+    # if (!is.na(input$lowerx)){
+    #   xlow=input$lowerx
+    # }
+    # if (!is.na(input$upperx)){
+    #   xupper=input$upperx
+    # }
+    # if (!is.na(input$lowery)){
+    #   ylow=input$lowery
+    # }
+    # if (!is.na(input$uppery)){
+    #   yupper=input$uppery
+    # }
+    # p <- p+xlim(xlow, xupper) + ylim(ylow, yupper) 
+    # p
     })
   
   output$info <- renderPrint({
-    brushedPoints(plot.data(), input$plot_brush)
+    if(is.null(ranges2$x)){
+      brushedPoints(plot.data(), input$plot1_brush)
+    } else {
+      D = plot.data()
+      D = subset(D, x.coord >= ranges2$x[1] & x.coord <= ranges2$x[2])
+      D = subset(D, y.coord >= ranges2$y[1] & y.coord <= ranges2$y[2])
+      print(D)
+    }
   })
   
+  output$info2 <- renderPrint({
+    res <- nearPoints(plot.data(), input$plot_click, "x.coord", "y.coord")
+    if (nrow(res) == 0)
+      return()
+    res
+    })
+  
   # to download plot
-  output$downloadPlot <- downloadHandler(
+  output$downloadPlot1 <- downloadHandler(
     
     # specify file name
     filename = function(){
@@ -104,15 +294,122 @@ shinyServer(function(input, output, session) {
     },
     content = function(filename){
       
-      # open device
-      pdf(filename)
       # plot
-      plot(D[input$columnx], D[input$columny], main="Scatterplot Example", 
-           xlab="x", ylab="y", pch=19)
-      # close device
-      dev.off()
+      D<-plot.data()
+      color <- plot.color()
+      
+      p <- ggplot(D, aes(x=x.coord, y=y.coord, color = color_code)) 
+      p <- p +  geom_point(size=input$scex, shape=as.integer(input$pch) ) 
+      if(input$colortype == 'continious'){
+        if(as.logical(input$showLegend)){
+          p <- p + scale_color_gradient(low=color[1], high=color[2], name = input$factors)
+        } else {
+          p <- p + scale_color_gradient(low=color[1], high=color[2], guide = as.logical(input$showLegend))
+        }
+      } else {
+        if(as.logical(input$showLegend)){
+          p <- p + scale_color_manual(values = color, name = input$factors)
+        } else {
+          p <- p + scale_color_manual(values = color, guide = as.logical(input$showLegend))
+        }
+      }
+      p <- p + ggtitle(input$title) + theme(plot.title = element_text(hjust = 0.5)) 
+      p <- p + xlab(input$xlabel) + ylab(input$ylabel)
+      
+      if(input$showGrid == TRUE){
+        p <- p + theme_bw() 
+      } else {
+        p <- p + theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+      }
+      p <- p + theme(plot.title = element_text(hjust = 0.5, size = input$fontsize.title),
+                     axis.text = element_text(colour="black", size = input$fontsize.axis),
+                     axis.title = element_text(colour="black", size = input$fontsize.axis),
+                     legend.text = element_text(colour="black", size = input$fontsize.legend),
+                     legend.title = element_text(colour="black", size = input$fontsize.legend)
+      )
+      
+
+      # save plot
+      ggsave(filename, p, device = 'pdf')
     }
   )
+  
+  output$downloadPlot2 <- downloadHandler(
+    
+    # specify file name
+    filename = function(){
+      paste0(input$outfile, ".zoom.", gitversion(),'.pdf')
+    },
+    content = function(filename){
+      
+      # plot
+      D<-plot.data()
+      color <- plot.color()
+      
+      p <- ggplot(D, aes(x=x.coord, y=y.coord, color = color_code)) 
+      p <- p +  geom_point(size=input$scex, shape=as.integer(input$pch)) 
+      if(input$colortype == 'continious'){
+        if(as.logical(input$showLegend)){
+          p <- p + scale_color_gradient(low=color[1], high=color[2], name = input$factors)
+        } else {
+          p <- p + scale_color_gradient(low=color[1], high=color[2], guide = as.logical(input$showLegend))
+        }
+      } else {
+        if(as.logical(input$showLegend)){
+          p <- p + scale_color_manual(values = color, name = input$factors)
+        } else {
+          p <- p + scale_color_manual(values = color, guide = as.logical(input$showLegend))
+        }
+      }
+      p <- p + ggtitle(input$title) + theme(plot.title = element_text(hjust = 0.5)) 
+      p <- p + xlab(input$xlabel) + ylab(input$ylabel)
+      print(paste("ranges2:", ranges2$x))
+      if(is.null(ranges2$x)){
+        p <- p + coord_cartesian(xlim = ranges1$x, ylim = ranges1$y, expand = FALSE)
+      } else {
+        p <- p + coord_cartesian(xlim = ranges2$x, ylim = ranges2$y, expand = FALSE)
+      }
+      
+      if(input$showGrid == TRUE){
+        p <- p + theme_bw() 
+      } else {
+        p <- p + theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+      }
+      p <- p + theme(plot.title = element_text(hjust = 0.5, size = input$fontsize.title),
+                     axis.text = element_text(colour="black", size = input$fontsize.axis),
+                     axis.title = element_text(colour="black", size = input$fontsize.axis),
+                     legend.text = element_text(colour="black", size = input$fontsize.legend),
+                     legend.title = element_text(colour="black", size = input$fontsize.legend)
+      )
+
+      # save plot
+      ggsave(filename, p, device = 'pdf')
+
+    }
+  )
+  
+  
+  output$downloadTable <- downloadHandler(
+    filename = function() {
+      paste(input$outfile,".filtered_table.",gitversion(),".tsv", sep = "")
+    },
+    content = function(filename) {
+      inFile <- input$uploaded_file
+      
+      if (is.null(inFile))
+        return(NULL)
+      if(is.null(ranges2$x)){
+        D <- brushedPoints(plot.data(), input$plot1_brush)
+      } else {
+        D = plot.data()
+        D = subset(D, x.coord >= ranges2$x[1] & x.coord <= ranges2$x[2])
+        D = subset(D, y.coord >= ranges2$y[1] & y.coord <= ranges2$y[2])
+        print(D)
+      }
+      write.table(D, filename, row.names = FALSE, quote = FALSE, sep = '\t')
+    }
+  )
+  
   
   output$appversion <- renderText ({ 
     paste0('App version: <b>',gitversion(),'</b>')
--- scatterplot/ui.R
+++ scatterplot/ui.R
@@ -9,49 +9,89 @@ symbols<-c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,
            106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127)
 
 
+
 shinyUI( fluidPage(
+  titlePanel("Scatterplot"),
   sidebarLayout(
     sidebarPanel(
-      fileInput("file1", "Choose File",
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv",
-                           ".tsv",
-                           ".xlsx")
-      ),
-      radioButtons("filetype", 'Please select the input file type', choices = c('auto' = 'auto', 
-                                                                                "excel" = 'xlsx',  
-                                                                                'tab-separated' = '\t', 
-                                                                                'comma-seperated' = ',', 
-                                                                                'semicolon-separated' = ';'), inline = TRUE),
-      checkboxInput("header", "Header", TRUE),
-      selectInput("columnx","Select X-axis", choices = c("--select--"),selected = "--select--"),
-      selectInput("columny","Select Y-axis", choices = c("--select--"),selected = "--select--"),
-      helpText(a(href = "https://raw.githubusercontent.com/mpg-age-bioinformatics/shiny/master/histogram/chol.txt", "Example input")),
-      hr(),
-      numericInput("lowerx", "X-axis lower limit", value = NA),
-      numericInput("upperx", "X-axis upper limit", value = NA),
-      numericInput("lowery", "Y-axis lower limit", value = NA),
-      numericInput("uppery", "Y-axis upper limit", value = NA),
-      textInput("title", "Plot title", value="Scatter plot"),
-      textInput("xlabel", "X-axis label", "x-axis"),
-      textInput("ylabel", "Y-axis label", "y-axis"),
-      selectInput("pch","Symbols", choices = symbols, selected = 19),
-      helpText(a(href = "http://sape.inf.usi.ch/quick-reference/ggplot2/shape", "Symbols map")),
-      sliderInput('scex', 'Symbols size', min = 0.1, max = 10, value = 2, step = 0.1),
-      hr(),
-      textInput("outfile", "Output file name", value="Scatterplot")#,
-      #submitButton('generate plot')
+      tabsetPanel(
+        tabPanel("Input Data",
+                 fileInput("uploaded_file", "Choose File",
+                  accept = c("text/csv",
+                             "text/comma-separated-values,text/plain",
+                             ".csv",
+                             ".tsv",
+                             ".xlsx")
+          ),
+          radioButtons("filetype", 'Please select the input file type', choices = c('auto' = 'auto', 
+                                                                                    "excel" = 'xlsx',  
+                                                                                    'tab-separated' = '\t', 
+                                                                                    'comma-seperated' = ',', 
+                                                                                    'semicolon-separated' = ';'), inline = TRUE),
+          checkboxInput("header", "Header", TRUE),
+          uiOutput("x"),
+          uiOutput("y"),
+          helpText(a(href = "https://raw.githubusercontent.com/mpg-age-bioinformatics/shiny/master/scatterplot/test_dataframe.csv", "Example input")),
+          hr(),
+          textInput("outfile", "Output file name", value="Scatterplot")#,
+          #submitButton('generate plot')
+          
+        ),
+        tabPanel("Plot settings", 
+               
+          # numericInput("lowerx", "X-axis lower limit", value = NA),
+          # numericInput("upperx", "X-axis upper limit", value = NA),
+          # numericInput("lowery", "Y-axis lower limit", value = NA),
+          # numericInput("uppery", "Y-axis upper limit", value = NA),
+          textInput("title", "Plot title", value="Scatter plot"),
+          textInput("xlabel", "X-axis label", "x-axis"),
+          textInput("ylabel", "Y-axis label", "y-axis"),
+          sliderInput("fontsize.title", "Title font size", min = 8, max = 32, value = 11.5, step = 0.5),
+          sliderInput("fontsize.axis", "Axes font size", min = 8, max = 32, value = 11.5, step = 0.5),
+          sliderInput("fontsize.legend", "Legend font size", min = 8, max = 32, value = 11.5, step = 0.5),
+          selectInput("pch","Symbols", choices = symbols, selected = 19),
+          radioButtons('showGrid', "Show grid", choices = c("yes" = TRUE, "no" = FALSE), selected = TRUE, inline = TRUE),
+          helpText(a(href = "http://sape.inf.usi.ch/quick-reference/ggplot2/shape", "Symbols map")),
+          sliderInput('scex', 'Symbols size', min = 0.1, max = 10, value = 2, step = 0.1),
+          radioButtons('colortype', "Please select variable type", choices = c('single','factor', 'continious'), inline = TRUE, selected = 'single'),
+          uiOutput("factors"),
+          textInput("color", 'Comma seperated list of colors', value = 'black'),
+          radioButtons("showLegend", "Show legend", choices = c("yes" = TRUE, "no" = FALSE), selected = FALSE, inline = TRUE),
+          hr()
+        )
+      )
     ),
     mainPanel(
-      #textOutput("testtext"),
-      plotOutput("scatterplot", height = "500px", width = "500px", brush = "plot_brush", click = "plot_click"),
-      verbatimTextOutput("info"),
-      downloadButton('downloadPlot', 'Download Plot'),
+             fluidRow(
+               column(width = 6, height = 6,
+                      h5("Use brush to zoom, displayed in right plot ->"),
+                      plotOutput("scatterplot1", click = "plot_click" ,
+                                 brush = brushOpts(
+                                   id = "plot1_brush",
+                                   resetOnNew = TRUE
+                                 )
+                      )
+               ),
+               column(width = 6, height = 6,
+                      h5("Use brush and double click to further zoom in"),
+                      plotOutput("scatterplot2", click = "plot_click", dblclick = "plot2_dblclick",
+                                brush = brushOpts(
+                                  id = "plot2_brush",
+                                  resetOnNew = TRUE) )
+               )
+             ),
+      #),
+      hr(),
+      downloadButton('downloadPlot1', 'Download plot'),
+      downloadButton('downloadPlot2', 'Download zoomed plot'),
+      downloadButton('downloadTable', "Download table"),
       br(), br(),
-      p("This App uses R's ", code('hist'), " function. For more information read the respective documentation in ",
-        a("rdocumentation.org", href = "https://www.rdocumentation.org/packages/graphics/versions/3.5.1/topics/hist"),
-        "and wikipedia's entry for ", a("histogram.",href="https://en.wikipedia.org/wiki/Histogram" )),
+      h4("Clicked points"),
+      verbatimTextOutput("info2", placeholder = TRUE),
+      h4("Brushed points"),
+      verbatimTextOutput("info", placeholder = TRUE),
+      p("This App uses R's ", code('ggplot2'), " package For more information read the respective documentation on ",
+        a("cran", href = "https://cran.r-project.org/web/packages/ggplot2/index.html"), "."),
       p("Please keep the version tag on all downloaded files."),
       htmlOutput('appversion')
     )
